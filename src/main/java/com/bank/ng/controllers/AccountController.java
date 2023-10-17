1fef4bbdde2fe1ccf8910c35e5023c232f60944a
kimng1003 <kimnagyeong50@gmail.com>
2023-10-17 13:30:26 +0900

    first commit

--- src/main/java/com/bank/ng/controllers/AccountController.java
+++ src/main/java/com/bank/ng/controllers/AccountController.java
@@ -3,10 +3,19 @@ package com.bank.ng.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
 public class AccountController {
+
+    private final String OPEN_BANKING_API_URL = "URL";
+    private final RestTemplate restTemplate;
+
+    public AccountController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
 
     @GetMapping("/api/accounts")
     public String getAccounts(){
